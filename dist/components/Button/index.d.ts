d09e9acc86ef5b3d65a9718eef05640313451ac2
GabbereX <[gabberex@mail.ru]>
2022-11-05 14:23:49 +0300

    webpack replaced by vite

--- dist/components/Button/index.d.ts
+++ dist/components/Button/index.d.ts
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import './styles.css';
 import { IButtonProps } from './types';
 declare const Button: FC<IButtonProps>;
 export default Button;
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'GBX-Lib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `gbx-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: [ 'react', 'react-dom', 'styled-components' ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+        },
+      },
+    },
+  },
+});
